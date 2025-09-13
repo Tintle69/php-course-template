3eb98a28d51cb143af7acf3e86b9c69636b9d926
Tintle69 <milordik43@mail.ru>
2025-09-13 09:59:13 +0000

    DZ

---
+++ sites/skuit/www/DZ/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Карабанов Алексей Анатольевич</title>
+</head>
+<body> 
+<h1>Hello World!!! 13.06.2008</h1>
+<div>
+<p>
+Трап
+</p>
+<p>
+Артур 
+</p>
+<p>
+Ссылка худ2худ
+<a href="https://www.merchbar.com/uncategorized/wormganger">Тут крутые мужики</a>
+</p>
+</div>
+</body>
+</html>
