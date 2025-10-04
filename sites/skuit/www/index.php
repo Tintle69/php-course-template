aa327f5a8434c5b6f624ee89d0d77b7d6c1bdd31
Tintle69 <milordik43@mail.ru>
2025-10-04 12:36:40 +0000

    commit -m

--- sites/skuit/www/index.php
+++ sites/skuit/www/index.php
@@ -1,3 +1,5 @@
 <?
-include_once './DZ/index.php';
+include_once './lab5/header.php';
+include_once './lab5/main.php';
+include_once './lab5/footer.php';
 ?>
---
+++ sites/skuit/www/lab5/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<title>Карабанов Алексей Анатольевич С025</title>
+<link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+<header>
+Деревня против Мордора
+</header>
+
+<div class="container">
